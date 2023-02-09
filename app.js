05232910f9247659f9e1be77f27f1f4d54739d73
Andrius Kasovskis <kasowskis@gmail.com>
2023-02-09 17:56:55 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// const fs = require('fs');
+const express = require("express")
+const app = express()
+const ytdl = require('ytdl-core');
+const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+app.listen(1000)
+
+app.post('/youtubeDownload', async (req, res) => {
+    const url = req.body.url
+
+    if(url.length > 500) return res.send({error: true, message: "Link too long"})
+
+    let info
+    try {
+        info = await ytdl.getInfo(url)
+    } catch (e) {
+        res.send({error: true, message: "Video unavailable. Check if link provided is correct"})
+    }
+
+    res.send(info)
+})
